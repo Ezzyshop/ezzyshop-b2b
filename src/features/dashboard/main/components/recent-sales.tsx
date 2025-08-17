ea79db9c61004cd080b112f32d6928329622364a
Ulugbek-desktop <ulugb3k270@gmail.com>
2025-08-17 18:35:16 +0500

    Refactor RecentSales component to fetch data dynamically and enhance localization support
    - Replaced static recent sales data with a dynamic API query to fetch recent orders, improving data accuracy and user experience.
    - Integrated loading and empty state handling with a skeleton loader and no sales message.
    - Updated localization files to include new keys for recent sales and no sales messages in Uzbek, enhancing user accessibility.

--- src/features/dashboard/main/components/recent-sales.tsx
+++ src/features/dashboard/main/components/recent-sales.tsx
@@ -1,70 +1,76 @@
+import { getOrdersQueryFn } from "@/api/queries";
 import { Avatar } from "@/components/ui/avatar";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useShopContext } from "@/contexts";
 import { AvatarFallback } from "@radix-ui/react-avatar";
-
-const recentSales = [
-  {
-    initials: "OM",
-    name: "Olivia Martin",
-    email: "olivia.martin@email.com",
-    amount: "+$1,999.00",
-  },
-  {
-    initials: "JL",
-    name: "Jackson Lee",
-    email: "jackson.lee@email.com",
-    amount: "+$39.00",
-  },
-  {
-    initials: "IN",
-    name: "Isabella Nguyen",
-    email: "isabella.nguyen@email.com",
-    amount: "+$299.00",
-  },
-  {
-    initials: "WK",
-    name: "William Kim",
-    email: "will@email.com",
-    amount: "+$99.00",
-  },
-  {
-    initials: "SD",
-    name: "Sofia Davis",
-    email: "sofia.davis@email.com",
-    amount: "+$39.00",
-  },
-];
+import { useQuery } from "@tanstack/react-query";
+import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
 
 export const RecentSales = () => {
+  const { t } = useTranslation();
+  const { shop } = useShopContext();
+
+  const params = {
+    limit: 5,
+    sortBy: "createdAt",
+    sortOrder: "desc",
+  };
+
+  const { data, isLoading } = useQuery({
+    queryKey: ["orders", shop._id],
+    queryFn: () => getOrdersQueryFn(shop._id, params),
+    enabled: !!shop?._id,
+  });
+
+  const renderContent = () => {
+    if (isLoading) {
+      return <Skeleton className="h-[118px] w-full" />;
+    }
+
+    if (data?.data.length === 0) {
+      return (
+        <div className="text-center text-sm text-gray-500  flex items-center justify-center h-[286px]">
+          {t("dashboard.main.no_sales")}
+        </div>
+      );
+    }
+
+    return data?.data.map((order) => (
+      <Link
+        to={`/dashboard/orders/${order._id}`}
+        key={order._id}
+        className="flex items-center"
+      >
+        <Avatar className="h-9 w-9 min-w-9">
+          <AvatarFallback className="text-sm font-medium flex items-center justify-center w-full bg-background">
+            {order.customer_info.name.charAt(0)}
+          </AvatarFallback>
+        </Avatar>
+        <div className="ml-4 space-y-1 flex-1 min-w-0">
+          <p className="text-sm font-medium leading-none">
+            {order.customer_info.name}
+          </p>
+          <p className="text-sm text-gray-500 truncate">
+            {order.customer_info.phone}
+          </p>
+        </div>
+        <div className="ml-auto font-medium text-sm">
+          + {order.total_price.toLocaleString()}{" "}
+          {order.transaction.currency.symbol}
+        </div>
+      </Link>
+    ));
+  };
+
   return (
     <Card className="md:col-span-3">
       <CardHeader>
-        <CardTitle>Recent Sales</CardTitle>
-        <CardDescription>You made 265 sales this month.</CardDescription>
+        <CardTitle>{t("dashboard.main.recent_sales")}</CardTitle>
       </CardHeader>
-      <CardContent>
-        <div className="space-y-6">
-          {recentSales.map((sale, index) => (
-            <div key={index} className="flex items-center">
-              <Avatar className="h-9 w-9">
-                <AvatarFallback className="text-sm font-medium">
-                  {sale.initials}
-                </AvatarFallback>
-              </Avatar>
-              <div className="ml-4 space-y-1 flex-1 min-w-0">
-                <p className="text-sm font-medium leading-none">{sale.name}</p>
-                <p className="text-sm text-gray-500 truncate">{sale.email}</p>
-              </div>
-              <div className="ml-auto font-medium text-sm">{sale.amount}</div>
-            </div>
-          ))}
-        </div>
+      <CardContent className="h-full">
+        <div className="space-y-6 h-full">{renderContent()}</div>
       </CardContent>
     </Card>
   );
